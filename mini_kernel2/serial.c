28647d67f047ebd26cd818d74574489b2d049a95
Zhang Fuxin <fxzhang@ict.ac.cn>
2022-05-14 10:24:04 +0800

    enable O2 optimization for mini kernels use global variable as uart base to work around a gcc 12 bug see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=99578 for details.

--- mini_kernel2/serial.c
+++ mini_kernel2/serial.c
@@ -3,10 +3,10 @@
 #include "serial.h"
 
 //static const unsigned long base = 0x900000001fe001e0ULL;
-static const unsigned long base = 0x1fe001e0;
+unsigned long uart_base = 0x1fe001e0;
 
-#define UART0_THR  (base + 0)
-#define UART0_LSR  (base + 5)
+#define UART0_THR  (uart_base + 0)
+#define UART0_LSR  (uart_base + 5)
 #define LSR_TX_IDLE  (1 << 5)
 
 static char io_readb(unsigned long addr)
